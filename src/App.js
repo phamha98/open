b19433052cd035bc699e27e9bfd253784d98688a
phamha98 <minhtruongpham.95@gmail.com>
2024-06-17 09:53:31 +0700

    add

--- src/App.js
+++ src/App.js
@@ -2,16 +2,10 @@ import './style.css';
 import './App.css';
 import { data as Data } from './data';
 import { useEffect, useState } from 'react';
-
+import Microlink from '@microlink/react';
 function App() {
   const [searchText, setSearchText] = useState('');
   const [results, setResults] = useState(Data);
-
-  useEffect(() => {
-    // Code to fetch data when the component mounts
-    // For example, you can fetch data from an API here
-  }, []);
-
   const handleSearch = (event) => {
     const txt = event.target.value.toLowerCase();
     setSearchText(txt);
@@ -26,9 +20,6 @@ function App() {
     });
     setResults(filteredData)
   };
-
-
-
   return (
     <div className="App">
       <div class="container">
@@ -47,30 +38,40 @@ function App() {
     </div>
   );
 }
-const copyToClipboard = (text) => {
-  const textarea = document.createElement('textarea');
-  textarea.value = text;
-  textarea.style.position = 'fixed'; // Ensure it's out of view
-  document.body.appendChild(textarea);
-  textarea.select();
-  document.execCommand('copy'); // Copy the text to clipboard
-  document.body.removeChild(textarea); // Clean up
-};
+
 
 const Item = ({ item }) => {
   if (Array.isArray(item)) {
     const [command, ...data] = item
+    const [links, codes] = data.reduce(([links, codes], str) => {
+      str.includes('http') ? links.push(str) : codes.push(str);
+      return [links, codes];
+    }, [[], []]);
     return (
-      <div>
+      <div style={{ marginTop: 15 }}>
         <li >
           <code>{command}</code>
           <button className="copy-button" onClick={() => copyToClipboard(command)}>
             <i className="fas fa-copy"></i>
           </button>
         </li>
-        {data.length > 0 && data.map((more, index) =>
-          <code key={index} style={{ fontSize: 12, color: 'gray' }}>{more}</code>
-        )}
+        <div style={{ display: data.length > 0 ? 'flex' : 'none', paddingLeft: 10, marginTop: 10, marginBottom: 10 }}>
+          <div style={{ display: codes.length > 0 ? 'flex' : 'none', backgroundColor: 'transparent', textAlign: 'left', flexDirection: 'column' }}>
+            {codes.map((more, index) =>
+              <code key={index} style={{ fontSize: 12, color: 'gray' }}>{more}</code>
+            )}
+          </div>
+          <div style={{ display: links.length > 0 ? 'flex' : 'none', backgroundColor: 'transparent', textAlign: 'left', flexDirection: 'column' }}>
+            {links.map((more, index) =>
+              <div key={index} >
+                <a href={more} style={{ fontSize: 12, }}>{more}</a>
+                <Microlink url={more} direction={more} />
+              </div>
+            )}
+            <div  >
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
@@ -84,4 +85,13 @@ const Item = ({ item }) => {
   )
 
 }
+const copyToClipboard = (text) => {
+  const textarea = document.createElement('textarea');
+  textarea.value = text;
+  textarea.style.position = 'fixed'; // Ensure it's out of view
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy'); // Copy the text to clipboard
+  document.body.removeChild(textarea); // Clean up
+};
 export default App;
